93e6a61ba3d72e91e19245eb610d4c95a08042db
sid-stack <55102866+sid-stack@users.noreply.github.com>
2021-01-23 21:48:06 +0530

    Add files via upload

--- fuzzy_set.py
+++ fuzzy_set.py
@@ -34,26 +34,41 @@ def Difference(A,B):
     C = Intersection(A,_B, f=1)
     print('Difference: ', C)
 
-def Cartesian(A,B):
+def Cartesian(A,B,f=1):
     r = 0 
     c = 0
     import numpy as np
-    C = np.array([0,0,0], ndmin = 2)
+    C = np.zeros(len(A)*len(B)).reshape(len(A), len(B)) 
     for key_1 in A:
-        r = r+1
         c = 0
         for key_2 in B:
             if A[key_1]<B[key_2]:
+                # C[key_1][key_2] = A[key_1] 
                 C[r][c] = A[key_1]
             else:
+                # C[key_1][key_2] = B[key_2] 
                 C[r][c] = B[key_2]
             c = c+1
-    print('Cartesian Product: ',C)
+        r = r+1
+    if f == 0:
+        return C
+    print('Cartesian Product: \n',C)
 
+def Composition(A,B,C):
+    for key_1 in A:
+        l = []
+        res = []
+        for key_2 in B:
+            for key_3 in C:
+                l.append(max(min(A[key_1],B[key_2]), C[key_3]))
+            res.append(max(l))
+        print(res)
+
+C = {'a':0.5,'b':0.5,'c':0.5}
 Union(A,B)
 Intersection(A,B)
 Complement(A)
 Complement(B)
 Difference(A,B)
 Cartesian(A,B)
-
+Composition(A,B,C)
